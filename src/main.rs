e35987c63979ee11d87d5e63338f0c556b9c7183
Filip Pacurar <filip@pacurar.net>
2023-07-09 01:42:08 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use base64::{engine::general_purpose, Engine as _};
+use codec::{DefaultErrorHandler, TopDecode};
+use multiversx_chain_vm::{bech32, DebugApi};
+use multiversx_sc::{
+    api::ManagedTypeApi,
+    codec::derive::{NestedDecode, NestedEncode, TopDecode, TopEncode},
+    types::{BigUint, ManagedAddress},
+};
+use multiversx_sc_codec as codec;
+use serde::{Deserialize, Serialize};
+
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, Debug)]
+struct AdvertiseSpace<M: ManagedTypeApi> {
+    owner: ManagedAddress<M>,
+    paid_amount: BigUint<M>,
+    paid_until: BigUint<M>,
+    is_new: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DecodedAdvertiseSpace {
+    owner: String,
+    paid_amount: u64,
+    paid_until: u64,
+    is_new: bool,
+}
+
+fn main() {
+    // first argument is base64 encoded string
+    let base64_encoded = std::env::args().nth(1).expect("no base64 encoded string");
+    // transform base64 encoded string to bytes
+    let bytes = general_purpose::STANDARD
+        .decode(base64_encoded)
+        .expect("invalid base64");
+
+    #[allow(unused_variables)]
+    let api = DebugApi::dummy();
+
+    // decode bytes to AdvertiseSpace struct
+    let decoded: AdvertiseSpace<DebugApi> =
+        AdvertiseSpace::top_decode_or_handle_err(bytes, DefaultErrorHandler).unwrap();
+
+    let addr = bech32::encode(&decoded.owner.to_address());
+
+    let final_decoded = DecodedAdvertiseSpace {
+        owner: addr,
+        paid_amount: decoded.paid_amount.to_u64().unwrap(),
+        paid_until: decoded.paid_until.to_u64().unwrap(),
+        is_new: decoded.is_new,
+    };
+
+    print!("{}", serde_json::to_string(&final_decoded).unwrap());
+}
